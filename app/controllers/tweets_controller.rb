b87731d8098fe3794c1ff55cff6716fea80f0159
AustinLinares <linaresaustin@hotmail.com>
2022-02-04 16:56:09 -0500

    tweets crud fixed

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,10 @@
 class TweetsController < ApplicationController
-  before_action :set_tweet, only: %i[show edit update destroy]
+  before_action :set_tweet, only: %i[show destroy edit update]
 
   # GET /tweets
   def index
     @tweets = Tweet.all
-    @new_tweet = Tweet.new
+    @tweet = Tweet.new
   end
 
   # GET /tweets/1
@@ -20,30 +20,22 @@ class TweetsController < ApplicationController
 
   # POST /tweets
   def create
-    p "#########################################"
-    p "#########################################"
-    p params
-    p "#########################################"
-    p params["body"]
-    p "#########################################"
-    p tweet_params
-    p "#########################################"
-
-    @new_tweet = Tweet.new(tweet_params)
-    @new_tweet = current_user
-    if @new_tweet.save
-      redirect_to root_path, notice: "Tweet was successfully created."
+    @tweet = Tweet.new
+    @tweet.body = params["body"]
+    @tweet.user = current_user
+    if @tweet.save
+      redirect_to root_path, notice: "Tweet created", status: :created
     else
-      render :new, status: :unprocessable_entity
+      render root_path, notice: "Tweet could not be created", status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /tweets/1
   def update
     if @tweet.update(tweet_params)
-      redirect_to @tweet, notice: "Tweet was successfully updated."
+      redirect_to root_path, notice: "Tweet successfully updated"
     else
-      render :edit, status: :unprocessable_entity
+      render :edit, notice: "Tweet could not be updated", status: :unprocessable_entity
     end
   end
 
@@ -62,6 +54,6 @@ class TweetsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def tweet_params
-    params.permit(:body)
+    params.require(:tweet).permit(:body)
   end
 end
