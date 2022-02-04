228bd19221ddfd4e6c498c13d48bdb622142b473
AustinLinares <linaresaustin@hotmail.com>
2022-02-04 10:27:46 -0500

    root route changed and validations and associations for user done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  # Associations
+  has_many :tweets, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  # Validations
+  validates :email, :username, :name, presence: true
+  validates :email, :username, uniqueness: true
+  validates :email,
+            format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/,
+                      message: "must have an email format" }
+  validates :encrypted_password, length: { minimun: 6 }
 end
