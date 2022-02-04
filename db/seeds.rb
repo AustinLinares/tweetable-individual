9e8156f62e236d8ffb98f88a728f5ea4de4bae5a
AustinLinares <linaresaustin@hotmail.com>
2022-02-04 10:48:13 -0500

    seed file starting

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+require 'open-uri'
+
+User.destroy_all
+Tweet.destroy_all
+Comment.destroy_all
+
+p "Start seeding users"
+puts "Creating admin"
+admin = User.new(
+  username: "@admin",
+  name: "Brandon Sanderson",
+  email: "admin@mail.com",
+  password: "supersecret",
+  role: 1
+)
+admin.avatar.attach(io: File.open("app/assets/images/image_profile_default.png"), filename: "admin.png")
+puts "Admin not created. Errors: #{admin.errors.full_messages}" unless admin.save
+
+
+puts "Seeding users"
+i = 1
+4.times do |member|
+  member = User.new
+  member.username = "@visitor#{i}"
+  member.name = Faker::Name.name
+  member.email = "visitor#{i}@mail.com"
+  member.password = "letmein"
+  member.avatar.attach(io: URI.open(Faker::Avatar.image(slug: "my-own-slug", size: "50x50")), filename: "visitor#{i}.jpg")
+  puts "Member not created. Errors: #{member.errors.full_messages}" unless member.save
+  i +=1
+end
